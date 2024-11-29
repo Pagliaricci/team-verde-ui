b36016e1d3e279d2d79bb39fbee07b8a55e593b2
FedeKraft <federicojkraft@gmail.com>
2024-11-29 16:14:42 -0300

    vmwriuvhiw

--- src/hooks/fetchAllUserSnippets.ts
+++ src/hooks/fetchAllUserSnippets.ts
@@ -3,7 +3,7 @@ import { Snippet } from '../utils/snippet.ts';
 
 export const fetchAllUserSnippets = async (id: string, token: string): Promise<Snippet[] | []> => {
     try {
-        const response = await axios.get('https://teamverde.westus2.cloudapp.azure.com/snippets', {
+        const response = await axios.get('http://snippets-service-infra:8080/snippets/', {
             headers: {
                 Authorization: `Bearer ${token}`,
             },

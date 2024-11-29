20d9e4d3757760b3733eafbdffd54dd135917295
FedeKraft <federicojkraft@gmail.com>
2024-11-29 17:27:07 -0300

    pasa papi2

--- src/hooks/fetchSnippetById.ts
+++ src/hooks/fetchSnippetById.ts
@@ -3,7 +3,7 @@ import {Snippet} from "../utils/snippet";
 
 export const fetchSnippetById = async (id: string, token: string): Promise<Snippet | null> => {
   try {
-    const response = await axios.get(`https://snippets-service-infra:8080/snippets/user/${id}`, {
+    const response = await axios.get(`https://teamverde.westus2.cloudapp.azure.com/snippets/user/${id}`, {
       headers: {
         Authorization: `Bearer ${token}`,
         "Content-Type": "application/json"

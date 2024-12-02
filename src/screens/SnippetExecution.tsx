cb11f8a19e7ab4394efc90ac5cbf72b4ec227f87
FedeKraft <federicojkraft@gmail.com>
2024-12-02 01:29:49 -0300

    hot fix snippet exec

--- src/screens/SnippetExecution.tsx
+++ src/screens/SnippetExecution.tsx
@@ -1,37 +1,36 @@
-import {OutlinedInput} from "@mui/material";
-import {highlight, languages} from "prismjs";
+import { OutlinedInput } from "@mui/material";
+import { highlight, languages } from "prismjs";
 import Editor from "react-simple-code-editor";
-import {Bòx} from "../components/snippet-table/SnippetBox.tsx";
-import {useEffect, useState} from "react";
+import { Bòx } from "../components/snippet-table/SnippetBox.tsx";
+import { useEffect, useState } from "react";
 
-export const SnippetExecution = ({errors}: { errors: string[] }) => {
-    // Here you should provide all the logic to connect to your sockets.
-    const [input, setInput] = useState<string>("")
+export const SnippetExecution = ({ errors = [] }: { errors?: string[] }) => {
+    const [input, setInput] = useState<string>("");
     const [output, setOutput] = useState<string[]>([]);
 
-    const code = output.join("\n")
+    const code = output.join("\n");
 
     const handleEnter = (event: { key: string }) => {
-        if (event.key === 'Enter') {
-            setOutput([...output, input])
-            setInput("")
+        if (event.key === "Enter") {
+            setOutput([...output, input]);
+            setInput("");
         }
     };
 
     useEffect(() => {
-        if (errors.length > 0) {
+        if (errors?.length > 0) {
             setOutput((prevOutput) => [...prevOutput, ...errors]);
         }
     }, [errors]);
 
     return (
         <>
-            <Bòx flex={1} overflow={"none"} minHeight={200} bgcolor={'black'} color={'white'} code={code}>
+            <Bòx flex={1} overflow="none" minHeight={200} bgcolor="black" color="white" code={code}>
                 <Editor
                     value={code}
                     padding={10}
                     onValueChange={(code) => setInput(code)}
-                    highlight={(code) => highlight(code, languages.js, 'javascript')}
+                    highlight={(code) => highlight(code, languages.js, "javascript")}
                     maxLength={1000}
                     style={{
                         fontFamily: "monospace",
@@ -39,8 +38,13 @@ export const SnippetExecution = ({errors}: { errors: string[] }) => {
                     }}
                 />
             </Bòx>
-            <OutlinedInput onKeyDown={handleEnter} value={input} onChange={e => setInput(e.target.value)}
-                           placeholder="Type here" fullWidth/>
+            <OutlinedInput
+                onKeyDown={handleEnter}
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                placeholder="Type here"
+                fullWidth
+            />
         </>
-    )
-}
+    );
+};
